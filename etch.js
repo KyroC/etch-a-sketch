140c59203e148872d881a415c017b48fec762408
KyroC <kyroeth@gmail.com>
2022-06-23 02:13:36 -0700

    Added gridSize() function

--- etch.js
+++ etch.js
@@ -4,21 +4,39 @@ const div = document.createElement('div')
 
 
 div.classList.add("grid-sq");
-div.textContent = "1"
+div.textContent = "1";
 
+container.style.cssText = "grid-template-columns: repeat(8, 1fr)"
 
+function trail(){
+    const grids = document.querySelectorAll(".grid-sq")
+    grids.forEach(grid => {
+    
+        grid.addEventListener('mouseover', function sketch() {
+            grid.classList.add("grid-sq-black")
+        })
+    });
+}
+function start() {
+    for (i = 0; i < 64; i++){
 
+        container.appendChild(div.cloneNode())
+    }
+    
 
-for (i = 0; i < 64; i++){
-
-    container.appendChild(div.cloneNode())
+    trail()
+   
 }
 
-const grids = document.querySelectorAll(".grid-sq")
+start()
 
-grids.forEach(grid => {
 
-    grid.addEventListener('mouseover', function sketch() {
-        grid.classList.add("grid-sq-black")
-    })
-});
+const sizeInput = document.getElementById("sizeInput")
+function gridSize() {
+    container.style.cssText = `grid-template-columns: repeat(${sizeInput.value}, 1fr)`
+    container.innerHTML = '';
+    for (i = 0; i < sizeInput.value *sizeInput.value; i++){
+        container.appendChild(div.cloneNode())
+    }
+    trail()
+}
